ccc36e8006d32124bdae6f8e5d2613a050408b9f
Francisco Luz <drupalista.com.br@gmail.com>
2025-12-19 02:43:41 -0400

    Removed $ variable

--- cheerio.js
+++ cheerio.js
@@ -3,5 +3,4 @@ const fs = require('fs');
 const arg = require('bot-arg');
 const [,,filepath_html = arg('filepath_html')] = process.argv;
 const html = fs.readFileSync(filepath_html, 'utf8');
-const $ = cheerio.load(html);
-module.exports = $;
+module.exports = cheerio.load(html);
